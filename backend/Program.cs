d16f7bed6eef64529408b52385925ab1fc6a79d0
tirey93 <tirey93@gmail.com>
2025-01-13 19:53:01 +0100

    added dockerfile

--- backend/Program.cs
+++ backend/Program.cs
@@ -18,12 +18,8 @@ builder.Services.AddCors(options => {
 var app = builder.Build();
 app.UseCors("cors");
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
